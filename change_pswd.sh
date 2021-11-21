51111b5b8151b90d5965bc281a5baa22dd01f8ab
anusha@peddoju <83536141+anusha-peddoju@users.noreply.github.com>
2021-11-21 18:25:19 +0530

    Add files via upload

---
+++ change_pswd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+password=$(zenity --password --title="Authentication") #Reading Password from User
+echo
+sed "s/user/$password/g" key.sh > setlock #Replacing current password in key.sh file and putting it in setlock file.
+rm key.sh #removing key.sh file
+sudo chmod +x  setlock #giving excutable permission to setlock
+sudo mv setlock /usr/bin #moving setlock to /usr/bin Directory
+sudo echo setlock  >> $HOME/.bashrc #adding setlock file to .bashrc file in home to run start in terminal.
+#here bashrc file only runs the file present in /usr/bin.
+sudo rm -r ../set_terminal_lock_zenity #removing set_terminal lock directory.
+
