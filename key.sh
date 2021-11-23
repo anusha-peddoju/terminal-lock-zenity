d3eef8ad8fc3eb57b7c75cb9154e0143fedac019
anusha@peddoju <83536141+anusha-peddoju@users.noreply.github.com>
2021-11-23 21:18:54 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 password="$(zenity --password --title="Authentication")"
-
-echo
-if [[ (md5sum <<< $password) == (md5sum <<< "user") ]] #checking password.here we will change user with original password later.
-then
-	sleep 1s
-	figlet "Welcome"
+start()
+{
+	echo
+	if [[ (md5sum <<< $password) == (md5sum <<< "user") ]] #checking password.here we will change user with original password later.
+	then
+		sleep 1s
+		figlet "Welcome"
+	else
+		sleep 1s
+		figlet "enter correct password"
+		sleep 3s
+		kill $(pgrep terminal) #Killing terminal
+	fi
+}
+dpkg -l md5sum > abc.txt
+if [[$?==0]]
+then 
+	start
 else
-	sleep 1s
-	figlet "enter correct password"
-	sleep 3s
-	kill $(pgrep terminal) #Killing terminal
-fi
-
+	sudo apt install -y ucommon-utils
+	start
