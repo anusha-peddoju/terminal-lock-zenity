b49f2f7957568c43f10d7758ff65c995708754d4
anusha@peddoju <83536141+anusha-peddoju@users.noreply.github.com>
2021-11-23 21:26:10 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -3,7 +3,7 @@ password="$(zenity --password --title="Authentication")"
 start()
 {
 	echo
-	if [[ (md5sum <<< $password) == (md5sum <<< "user") ]] #checking password.here we will change user with original password later.
+	if [[ ~md5sum <<< $password` == `md5sum <<< "user"` ]] #checking password.here we will change user with original password later.
 	then
 		sleep 1s
 		figlet "Welcome"
