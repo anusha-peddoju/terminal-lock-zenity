b05274ffc2272bececeb60b6c293c142f0469cd6
anusha@peddoju <83536141+anusha-peddoju@users.noreply.github.com>
2021-11-25 11:10:19 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 password="$(zenity --password --title="Authentication")"
-start()
-{
-	echo
-	if [[ `md5sum <<< $password` == `md5sum <<< "user"` ]] #checking password.here we will change user with original password later.
-	then
-		sleep 1s
-		figlet "Welcome"
-	else
-		sleep 1s
-		figlet "enter correct password"
-		sleep 3s
-		kill $(pgrep terminal) #Killing terminal
-	fi
-}
+
+echo
+if [[ $password == "user" ]] #checking password.here we will change user with original password later.
+then
+	sleep 1s
+	figlet "Welcome"
+else
+	sleep 1s
+	figlet "enter correct password"
+	sleep 3s
+	kill $(pgrep terminal) #Killing terminal
+fi
+
 
