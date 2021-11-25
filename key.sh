cdde4963c7f18a58864708b76e80df8f8957b0ec
anusha@peddoju <83536141+anusha-peddoju@users.noreply.github.com>
2021-11-25 11:05:35 +0530

    Update key.sh

--- key.sh
+++ key.sh
@@ -14,15 +14,4 @@ start()
 		kill $(pgrep terminal) #Killing terminal
 	fi
 }
-dpkg -l md5sum > abc.txt
-if [[$?==0]]
-then 
-	start
-else
-	ping -c 3 google.com #pinging to google
-	if [ $? == 0 ]
-	then
-		sudo apt install -y ucommon-utils
-		start
-	else
-		echo "check your internet connection"
+
